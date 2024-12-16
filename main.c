639fae6827a245c28cc3602dc2dbb571bbc0aa1e
Matias Quero <mquero@c1r6p16.hive.fi>
2024-12-16 14:44:16 +0200

    checking error messages

--- main.c
+++ main.c
@@ -6,98 +6,152 @@
 /*   By: mquero <mquero@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 16:21:30 by mquero            #+#    #+#             */
-/*   Updated: 2024/12/15 20:57:50 by mquero           ###   ########.fr       */
+/*   Updated: 2024/12/16 14:37:04 by mquero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	*find_path(char *argv, char **envp)
+bool	check_if_full_path(char *arg)
 {
-    int i;
-    char **paths;
-    char *wholepath;
-
-    i = 0;
-    while (envp[i])
-    {
-        if (envp[i][0] == 'P' && envp[i][1] == 'A' && envp[i][2] == 'T')
-            paths = ft_split(envp[i] + 5, ':');
-        i++;
-    }
-    i = 0;
-    while (paths[i])
-    {
-        wholepath = ft_strjoin(paths[i], argv);
-        if (access(wholepath, X_OK) == 0)
-            break;
-        else
-            free(wholepath);
-        i++;
-    }
-    freesplit(paths);
-    return (wholepath);
+		if (access(arg, X_OK) == 0)
+			return (true);
+		else
+			return (false);
 }
 
-void    child1(t_fd fd, char **argv, char **envp)
+void	close_all(t_fd *fd)
 {
-    char    *path;
-    char    **split;
-
-    path = find_path(argv[2], envp);
-    split = ft_split(argv[2], ' ');
-    dup2(fd.input, STDIN_FILENO);
-    dup2(fd.pipe[1], STDOUT_FILENO);
-    close(fd.input);
-    close(fd.output);
-    close(fd.pipe[0]);
-    close(fd.pipe[1]);
-    execve(path, split, envp);
-    free(path);
-    freesplit(split);
+	close(fd->pipe[0]);
+	close(fd->pipe[1]);
+	close(fd->output);
+	close(fd->input);
 }
-void    child2(t_fd fd, char **argv, char **envp)
-{
-    char    *path;
-    char    **split;
 
-    path = find_path(argv[3], envp);
-    split = ft_split(argv[3], ' ');
-    dup2(fd.pipe[0], STDIN_FILENO);
-    dup2(fd.output, STDOUT_FILENO);
-    close(fd.input);
-    close(fd.output);
-    close(fd.pipe[0]);
-    close(fd.pipe[1]);
-    execve(path, split, envp);
-    freesplit(split);
-    free(path);
+char	*find_path(char *argv, char **envp, int i)
+{
+	char	**paths;
+	char	*wholepath;
+
+	while (envp[i])
+	{
+		if (envp[i][0] == 'P' && envp[i][1] == 'A' && envp[i][2] == 'T')
+			paths = ft_split(envp[i] + 5, ':');
+		i++;
+	}
+	i = 0;
+	while (paths[i])
+	{
+		wholepath = ft_strjoin(paths[i], argv);
+		if (access(wholepath, X_OK) == 0)
+			break ;
+		else
+		{
+			free(wholepath);
+			wholepath = NULL;
+		}
+		i++;
+	}
+	freesplit(paths);
+	return (wholepath);
+}
+
+void	child1(t_fd fd, char **argv, char **envp)
+{
+	char	*path;
+	char	**split;
+	int		i;
+
+	i = 0;
+	if (check_if_full_path(argv[2]))
+		path = argv[2];
+	else
+		path = find_path(argv[2], envp, i);
+	if (path == NULL)
+	{
+		close_all(&fd);
+        perror (argv[2]);
+		exit(1);
+	}
+	split = ft_split(argv[2], ' ');
+	dup2(fd.input, STDIN_FILENO);
+	dup2(fd.pipe[1], STDOUT_FILENO);
+	close_all(&fd);
+	if (execve(path, split, envp) == -1)
+    {
+        free(path);
+        freesplit(split);
+        strerror(errno);
+        exit(1);
+    }
+}
+
+void	child2(t_fd fd, char **argv, char **envp)
+{
+	char	*path;
+	char	**split;
+	int		i;
+
+	i = 0;
+	if (check_if_full_path(argv[3]))
+		path = argv[3];
+	else
+		path = find_path(argv[3], envp, i);
+	if (path == NULL)
+	{
+		close_all(&fd);
+        perror (argv[3]);
+		exit(1);
+	}
+    perror (argv[2]);
+	split = ft_split(argv[3], ' ');
+	dup2(fd.pipe[0], STDIN_FILENO);
+	dup2(fd.output, STDOUT_FILENO);
+	close_all(&fd);
+	if (execve(path, split, envp) == -1)
+    {
+		printf("dsadsa");
+		free(path);
+		freesplit(split);
+		strerror(errno);
+		exit(1);
+    }
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-    t_fd    fd;
+	t_fd	fd;
 	int		pid1;
 	int		pid2;
+    //int     fd_dir;
+    //int     fd_dir2;
 
-    if (argc != 5)
-    {
-        return 0;
-    }
+	if (argc != 5)
+		return (0);
+    /*fd_dir = open(argv[2], O_DIRECTORY);
+    fd_dir2 = open(argv[3], O_DIRECTORY);
+	if (fd_dir != -1)
+	{
+		close(fd_dir);
+		perror(NULL);
+	}
+	if (fd_dir2 != -1)
+	{
+		close(fd_dir2);
+		perror(NULL);
+	}*/
+
 	pipe(fd.pipe);
 	fd.input = open(argv[1], O_RDONLY);
 	fd.output = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	pid1 = fork();
 	if (pid1 == 0)
-        child1(fd, argv, envp);
+		child1(fd, argv, envp);
 	pid2 = fork();
 	if (pid2 == 0)
-        child2(fd, argv, envp);
-    close(fd.pipe[0]);
-    close(fd.pipe[1]);
-    close(fd.output);
-    close(fd.input);
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
+		child2(fd, argv, envp);
+	close_all(&fd);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -6,37 +6,38 @@
 /*   By: mquero <mquero@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 16:50:40 by mquero            #+#    #+#             */
-/*   Updated: 2024/12/15 16:54:46 by mquero           ###   ########.fr       */
+/*   Updated: 2024/12/16 13:48:02 by mquero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
+# include <fcntl.h>
 # include <limits.h>
 # include <stdint.h>
 # include <stdio.h>
+# include <stdbool.h>
 # include <stdlib.h>
+# include <sys/wait.h>
 # include <unistd.h>
-# include <fcntl.h>
-#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
 
 
 typedef struct s_fd
 {
 	int		pipe[2];
-	char	buffer[10000];
-    char    *path1;
-    char    *path2;
-    char    **split;
-    int input;
-    int output;
-}   t_fd;
+	char	*path1;
+	char	*path2;
+	char	**split;
+	int		input;
+	int		output;
+}			t_fd;
 
-
-char	*ft_strjoin(char const *s1, char const *s2);
-void	freesplit(char **strs);
-char	**ft_split(char const *s, char c);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char		*ft_strjoin(char const *s1, char const *s2);
+void		freesplit(char **strs);
+char		**ft_split(char const *s, char c);
+size_t		ft_strlcpy(char *dst, const char *src, size_t size);
 
 #endif
